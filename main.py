dae218d4ccce0fc88f67c355098d3b4a1afb2e11
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2023-11-02 11:47:05 -0500

    Added functionality for HH:MM:SS vs. Milliseconds

---
+++ main.py
@@ -0,0 +1,17 @@
+import subtitle_reader as sr
+if __name__ == "__main__":
+    # Intensifiers:
+    reader = sr.SubtitleReader(filename = 'intensifiers.txt')
+    reader.read_all_in_folder(input_path = "./input-files/", 
+                              output_path = "./output-files/", 
+                              time_in_ms = True)
+    # Like:
+    reader = sr.SubtitleReader(filename = 'like.txt')
+    reader.read_all_in_folder(input_path = "./input-files/", 
+                              output_path = "./output-files/", 
+                              time_in_ms = True, 
+                              verbose = True)
+    
+    # # The entire transcript:
+    # reader = sr.SubtitleReader(filename = 'everything.txt')
+    # reader.read_all_in_folder(input_path = "./input-files/", output_path = "./output-files/", time_in_ms = True)
